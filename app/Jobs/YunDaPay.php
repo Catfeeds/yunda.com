ec3cdf5e5f2b62b304cbf72f81bd83aaff11813e
wangshilei <wangsl@inschos.com>
2018-04-28 16:47:06 +0800

    '04.28'

--- app/Jobs/YunDaPay.php
+++ app/Jobs/YunDaPay.php
@@ -80,10 +80,9 @@ class YunDaPay implements ShouldQueue
     {
         //用户身份信息
         $input = $this->param;
-//        $input = '{"operate_code":"","channel_code":"YD","courier_state":"","courier_start_time":"","p_code":"","is_insure":"","channel_back_url":"","channel_user_name":"张耘赫","channel_user_code":"620103199012171917","channel_user_phone":"13186050625","channel_user_email":null,"channel_user_address":null,"channel_bank_code":"6217730704649985","channel_bank_name":null,"channel_bank_address":null,"channel_bank_phone":"13186050625","channel_provinces":null,"channel_city":null,"channel_county":null,"price":"2","insured_days":"1"}';
+		LogHelper::logChannelSuccess($input, 'YD_request_params');
         $requset_url = config('yunda.request_url');
         LogHelper::logChannelSuccess($requset_url, 'YD_request_url');
-        LogHelper::logChannelSuccess($input, 'YD_request_params');
         $response = Curl::to($requset_url)
             ->returnResponseObject()
             ->withData($input)
