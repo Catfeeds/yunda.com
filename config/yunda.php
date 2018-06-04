50ac7b16367a61e3dd635f234c89a7f83dec7b4a
wangshilei <wangsl@inschos.com>
2018-06-04 17:37:04 +0800

    '06.04'

--- config/yunda.php
+++ config/yunda.php
@@ -34,7 +34,7 @@ return [
     ],
     //邮件路径
     'email_url'=>'/webapi',
-    'file_url'=>'http://122.14.202.233:9100/api_file/',
+    'file_url'=>'//122.14.202.233:9100/api_file/',
     //产品ID对应邮箱地址
     'product_id_email'=>[
         '1'=>'ydgfkfb@163.com',
