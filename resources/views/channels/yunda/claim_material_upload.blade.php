67a22f4e8aa46382a9cb1ff12d2599e22793ff7b
tmy <tmingyy@qq.com>
2018-06-04 17:50:08 +0800

    '返回失效'

--- resources/views/channels/yunda/claim_material_upload.blade.php
+++ resources/views/channels/yunda/claim_material_upload.blade.php
@@ -162,10 +162,11 @@
 	@endif
 </div>
 <script type="text/javascript" src="{{config('view_url.channel_views')}}js/jquery-1.10.2.min.js"></script>
+<script src="{{config('view_url.channel_views')}}js/lib/mui.min.js"></script>
 <script type="text/javascript">
     $('body').on('click','.formW2 img',function(){
         $(this).parent().find('input').eq(0).click();
-    })
+    });
     // 上传照片
     var upLoadImg = function(e){
         var _this = $(e).parent();
