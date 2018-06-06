eec8bf00c96638bbc172606df170339aa3398fff
= <=>
2018-06-06 13:54:10 +0800

    06.06

--- public/r_channels/yunda/js/baidu.statistics.js
+++ public/r_channels/yunda/js/baidu.statistics.js
@@ -1,8 +1,9 @@
+<script>
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?040c92afc0bf23a572eb17192523ddea";
+  hm.src = "https://hm.baidu.com/hm.js?8f0202ed384645ece7c01d4084e881a0";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-
+</script>
